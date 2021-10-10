9c228f3752a1d0032e64fe345e19b44f7e9ecbfa
rojajullapalli <rojajules@gmail.com>
2021-10-10 19:36:42 +0530

    ADDED: DB Connection done

---
+++ src/main/java/com/example/addressbookapp/controller/AddressBookController.java
@@ -0,0 +1,10 @@
+package com.example.addressbookapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/addressbook")
+public class AddressBookController {
+
+}
